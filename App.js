0b23ec72b547bfdd01065a38a1fe448c110636ac
Keli Booster <kbooster17@gmail.com>
2021-09-14 20:35:11 +0000

    add music components

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
